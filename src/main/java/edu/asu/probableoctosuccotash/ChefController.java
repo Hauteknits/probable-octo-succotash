34cceb6313d3077e760cd65ab4c2cfaf96a7c431
Nsai-roshan <snelaval@asu.edu>
2022-11-13 04:53:17 -0700

    Chefcontroller
    So I am updating the order progress from 200 to 300 by using event handler

--- src/main/java/edu/asu/probableoctosuccotash/ChefController.java
+++ src/main/java/edu/asu/probableoctosuccotash/ChefController.java
@@ -1,8 +1,66 @@
 package edu.asu.probableoctosuccotash;
 
-public class ChefController extends OrderManager{
-    @Override
-    protected void updateScreen(int code) {
+import java.io.IOException;
 
-    }
-}
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class ChefController extends OrderManager{
+	
+	// State: 0 = new order, 100 = Processed, 200 = Cooking, 300 = Cooked, 400 = Killed/Picked Up
+	
+	private int orderID;
+	
+	@FXML
+    private Button cookedButton;
+
+	 public EventHandler<ActionEvent> Cooked = new EventHandler<ActionEvent>() {
+	@Override
+			public void handle(ActionEvent event) {
+				// TODO Auto-generated method stub
+				for(Order order : localOrders) {
+		    		if(orderID == order.getOrderID()) {
+		    			if(order.getState() == 200) {
+		    				order.updateStatus(300);
+		    			}
+		    		}
+				}
+				
+			}
+
+			
+	 };
+	        
+	        private void updatestatus() {
+	        	 cookedButton.setOnAction(Cooked);
+	        }
+	 
+   
+
+	@Override
+	protected void updateScreen(int code) {
+		// TODO Auto-generated method stub
+		for(Order order : localOrders) {
+    		if(orderID == order.getOrderID()) {
+    			if(order.getState() == 200) {
+    				order.updateStatus(300);
+    			}
+    		}
+    		}
+	}}
+
+    
+
+    	
+    
+    
+
+    
+
+	
